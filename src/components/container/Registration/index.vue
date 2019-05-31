b5ec35d8db7ffc21346eacc8d70e1e7b2469b116
Andrea <andy2890@gmail.com>
2019-05-31 17:33:27 +0530

    Added verifyRegistration Page

--- src/components/container/Registration/index.vue
+++ src/components/container/Registration/index.vue
@@ -2,21 +2,23 @@
 
 <template>
   <main>
-      <GetStarted/>
+     <GetStarted/>
+    <VerifyRegistration/>
   </main>
 </template>
 
 <script>
 const GetStarted = () => import('@/components/container/Registration/getStarted');
+const VerifyRegistration =() => import('@/components/container/Registration/verifyRegistration');
 
 export default {
   name: "Registration",
   components: {
     GetStarted,
+    VerifyRegistration
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/components/container/Registration/verifyRegistration.vue
@@ -0,0 +1,53 @@
+<i18n>
+{
+  "en": {
+    "title": "Products!",
+    "tab-title-1": "Exchange",
+    "tab-title-2": "Auction"
+  }
+}
+</i18n>
+
+<template>
+  <main>
+    <section class="dashboard">
+      <div class="container">
+          <a href='http://t.me/varun90bot' target='_blank'>Click Here To Continue</a>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "VerifyRegistration",
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  margin-bottom: 20px;
+}
+
+.container {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 0 15px;
+}
+
+.header-carousel {
+  .owl-carousel .owl-item img {
+    max-height: 200px;
+  }
+}
+
+.md-card {
+  width: 150px;
+  margin: 20px 5px;
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
